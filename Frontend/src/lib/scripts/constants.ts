ebb707e6f8ca2fac3d4287b8a06388b5debe976b
Meedo <96346713+Meedony@users.noreply.github.com>
2024-01-02 12:25:23 +0300

    Update constants.ts

--- Frontend/src/lib/scripts/constants.ts
+++ Frontend/src/lib/scripts/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_PATH = "http://backend.olympushosting.uk"
+export const BASE_PATH = "https://backend.olympushosting.uk"
 export const INVITE_URL = "https://invite.ree6.de"
 
 export function get(path: string) {
